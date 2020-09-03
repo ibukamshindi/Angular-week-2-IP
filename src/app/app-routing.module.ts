0aec0b936f5bc21fbd5314b7c22b63720408fd4e
ibukamshindi <ibukamshindi@gmail.com>
2020-09-03 10:16:30 +0300

    added route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import{AboutMeComponent} from './about-me/about-me.component';
+import {GitDisplayComponent} from './git-display/git-display.component'
 
 const routes: Routes = [
   {path:'aboutme',component:AboutMeComponent}
+  {path:'',component:GitDisplayComponent}
 ];
 
 @NgModule({
